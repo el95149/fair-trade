0e297e1edf704d10b13772e261c714c29ce2b1dc
Angelos Anagnostopoulos <angelos@anagnostic.org>
2018-04-01 17:43:28 +0300

    Fix broken logo link

--- fair-trade-client/src/components/Header.vue
+++ fair-trade-client/src/components/Header.vue
@@ -4,7 +4,7 @@
       <button class="navbar-toggler mobile-sidebar-toggler d-lg-none" type="button" @click="mobileSidebarToggle">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <b-link class="navbar-brand" to="calendar"></b-link>
+      <b-link class="navbar-brand" to="map"></b-link>
       <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button" @click="sidebarToggle">
         <span class="navbar-toggler-icon"></span>
       </button>

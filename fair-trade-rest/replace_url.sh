f59b9f771e1d4ae725247d8094cdc1db48692140
Angelos Anagnostopoulos <angelos@anagnostic.org>
2018-04-01 16:38:12 +0300

    Prepare production packaging

--- fair-trade-rest/replace_url.sh
+++ fair-trade-rest/replace_url.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "Replacing URL"
-sed -i "s|http://127.0.0.1:8080/|/|g" ../contracts-client/dist/static/js/app*
+sed -i "s|http://127.0.0.1:8080/|/|g" ../fair-trade-client/dist/static/js/app*
